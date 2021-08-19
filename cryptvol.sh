030baaed0448a30c205ec09c539cccbcb5dc825d
David S. Jackson <deepbsd@yahoo.com>
2021-08-18 20:45:32 -0400

    corrected typo

--- cryptvol.sh
+++ cryptvol.sh
@@ -259,9 +259,9 @@ HOSTS
 grub_load(){
     # Append grub default config
     echo 'GRUB_PRELOAD_MODULES="lvm"' >> /mnt/etc/default/grub
-    echo 'GRUB_ENABLE_CRYPTTODISK=y' >> /mnt/etc/default/grub
+    echo 'GRUB_ENABLE_CRYPTODISK=y' >> /mnt/etc/default/grub
     myuuid=$( lsblk -f | grep "${CRYPTVOL}" | head -n1 | awk '{ print $5 }'  )
-    echo GRUB_CMDLINE_LINUX="cryptdevice=UUID=$myuuid:cryptdata" >> /mnt/etc/default/grub
+    echo -e GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$myuuid:cryptdata\" >> /mnt/etc/default/grub
 }
 
 # SET ROOT PASSWORD
